e4ff8a208e693eef3cff61660952c26a9dad8cda
JoanJLlopis <67593934+JoanJLlopis@users.noreply.github.com>
2023-02-06 14:32:27 +0100

    irrelevant changes of syntax

--- src/components/UI-Components/Header.component.jsx
+++ src/components/UI-Components/Header.component.jsx
@@ -7,11 +7,11 @@ const Header = () => {
     return (
         <div>
             <div className="bg-red-500 p-2.5 flex items-center justify-between h-84">
-                <img src={MoonIcon} className="fas fa-lg fa-arrow-left ml-6 sm:ml-10 md:mr-15 lg:mr-25 xl:mr-35" ></img>
-                <img src={FavoritesIcon} className="fas fa-lg fa-arrow-right mr-6 sm:mr-10 md:mr-15 lg:mr-25 xl:mr-35" ></img>
+                <img src={MoonIcon} className="fas fa-lg fa-arrow-left ml-6 sm:ml-10 md:mr-15 lg:mr-25 xl:mr-35" />
+                <img src={FavoritesIcon} className="fas fa-lg fa-arrow-right mr-6 sm:mr-10 md:mr-15 lg:mr-25 xl:mr-35" />
             </div>
             <div className='flex justify-center'>
-                <img src={PokemonLogo} alt="Header Logo" className="w-48  sm:w-48 md:w-64 lg:w-70 xl:w-80   -mt-12 sm:-mt-12 md:-mt-16 lg:-mt-16 xl:-mt-20"></img> 
+                <img src={PokemonLogo} alt="Header Logo" className="w-48  sm:w-48 md:w-64 lg:w-70 xl:w-80   -mt-12 sm:-mt-12 md:-mt-16 lg:-mt-16 xl:-mt-20"/> 
             </div>
         </div>
 
--- src/components/UI-Components/SearchBar.component.jsx
+++ src/components/UI-Components/SearchBar.component.jsx
@@ -3,7 +3,7 @@ import searchIcon from '../../assets/Icons/SearchIcon.png'
 const SearchBar = () => {
     return (
         <div className='flex items-center justify-center'>
-            <div class="relative w-96">
+            <div className="relative w-96">
                 <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full pl-10 p-2.5 " placeholder="Search " ></input>
                 <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3">
                     <img src={searchIcon} className="w-8 h-8" alt="Search Icon"/>
